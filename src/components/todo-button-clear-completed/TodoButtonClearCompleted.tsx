ca50f070738cc4766a1871737cae75c26d326ce9
Galeed Gutierrez <galeedeveloper@gmail.com>
2024-06-23 03:43:57 -0300

    feat: :sparkles: We can clear completed tasks

--- src/components/todo-button-clear-completed/TodoButtonClearCompleted.tsx
+++ src/components/todo-button-clear-completed/TodoButtonClearCompleted.tsx
@@ -1,9 +1,25 @@
+import { TodoContext } from '@src/context/TodoContext';
+import { useContext } from 'react';
+
 export function TodoButtonClearCompleted(): JSX.Element {
+	const TODO_CONTEXT = useContext(TodoContext);
+
+	if (!TODO_CONTEXT) {
+		return <></>;
+	}
+
+	const { todos, setTodos } = TODO_CONTEXT;
+
 	return (
 		<button
 			type="button"
 			className="text-hover text-active text-color-2"
 			aria-label="Delete all completed tasks"
+			onClick={() => {
+				const NEW_TODOS = todos.filter((todo) => !todo.completed);
+
+				setTodos(NEW_TODOS);
+			}}
 		>
 			Clear Completed
 		</button>
